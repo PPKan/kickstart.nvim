8b8beb8be55c6b3241aa1296151113bc48a0ebe1
Peter <petervsjim@gmail.com>
2025-05-21 10:05:22 +0800

    make im-select don't clear buffer after save

--- init.lua
+++ init.lua
@@ -256,45 +256,35 @@ require('lazy').setup({
     },
   },
 
-  { -- im_select
-    'keaising/im-select.nvim',
-    config = function()
-      require('im_select').setup {
-        -- IM will be set to `default_im_select` in `normal` mode
-        -- For Windows/WSL, default: "1033", aka: English US Keyboard
-        -- For macOS, default: "com.apple.keylayout.ABC", aka: US
-        -- For Linux, default:
-        --               "keyboard-us" for Fcitx5
-        --               "1" for Fcitx
-        --               "xkb:us::eng" for ibus
-        -- You can use `im-select` or `fcitx5-remote -n` to get the IM's name
-        default_im_select = '1033',
+{ -- im_select
+  'keaising/im-select.nvim',
+  config = function()
+    require('im_select').setup {
+      default_im_select = '1033',          -- 英文 (US)
+      default_command   = 'C:\\tools\\im-select.exe',
 
-        -- Can be binary's name, binary's full path, or a table, e.g. 'im-select',
-        -- '/usr/local/bin/im-select' for binary without extra arguments,
-        -- or { "AIMSwitcher.exe", "--imm" } for binary need extra arguments to work.
-        -- For Windows/WSL, default: "im-select.exe"
-        -- For macOS, default: "macism"
-        -- For Linux, default: "fcitx5-remote" or "fcitx-remote" or "ibus"
-        default_command = 'C:\\tools\\im-select.exe',
+      -- 進/離 Insert 才交給插件處理
+      set_default_events  = { 'VimEnter', 'FocusGained', 'InsertLeave' },
+      set_previous_events = { 'InsertEnter' },
 
-        -- Restore the default input method state when the following events are triggered
-        set_default_events = { 'VimEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave' },
+      async_switch_im = true,
+    }
 
-        -- Restore the previous used input method state when the following events
-        -- are triggered, if you don't want to restore previous used im in Insert mode,
-        -- e.g. deprecated `disable_auto_restore = 1`, just let it empty
-        -- as `set_previous_events = {}`
-        set_previous_events = { 'InsertEnter' },
+    ------------------------------------------------------------------
+    -- 離開 command-line 時強制切回英文，但不覆寫 prev_im ----------
+    ------------------------------------------------------------------
+    vim.api.nvim_create_autocmd('CmdlineLeave', {
+      callback = function()
+        -- Windows / WSL
+        vim.fn.system('C:\\tools\\im-select.exe 1033')
 
-        -- Show notification about how to install executable binary when binary missed
-        keep_quiet_on_no_binary = false,
-
-        -- Async run `default_command` to switch IM or not
-        async_switch_im = true,
-      }
-    end,
-  },
+        -- macOS → vim.fn.system('macism com.apple.keylayout.ABC')
+        -- Fcitx5 → vim.fn.system('fcitx5-remote -c')
+        -- IBus   → vim.fn.system('ibus engine xkb:us::eng')
+      end,
+    })
+  end,
+},
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
